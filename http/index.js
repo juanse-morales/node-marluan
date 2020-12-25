088937c6c935dd56cc3b4e988cc2299954eccd70
Juan Morales <jmorales@dcatech.com>
2020-12-25 15:46:32 -0500

    Clase 48: Entendiendo las rutas con Express

--- http/index.js
+++ http/index.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const fs = require('fs');
+
 const server = express();
+
+const home = fs.readFileSync('./index.html');
+const about = fs.readFileSync('./about.html');
+
+server.get('/',(req, res)=>{
+  res.write(home);
+  res.end();
+});
+
+server.get('/about',(req, res)=>{
+  res.write(about);
+  res.end();
+});
+
 
 server.listen(8080, ()=>{
   console.log("Server is running on port "+ 8080);
