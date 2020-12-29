d73b0af54f0c9ea4ca59dd238d6ed2a058488aba
Juan Morales <jmorales@dcatech.com>
2020-12-29 10:08:54 -0500

    Clase 86: Experimentando la arquitectura

---
+++ api/intro.js
@@ -0,0 +1,12 @@
+/*
+
+API: Se trata de un conjunto de definiciones y protocolos que se utilizan para 
+desarrollar e integrar el software de las aplicaciones.
+
+Patrón Arquitectónico: Es una solución general y reutilizable a un problema común en la arquitectura de software
+dentro de un contexto dado.
+
+REST: un estilo de arquitectura basado en un conjunto de principios predefinidos que describen 
+cómo se definen y se abordan los recursos a través de un servicio web.
+
+*/
