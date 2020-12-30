d713f892cdce944ec2bc0b622ce7492be601bab5
Juan Morales <jmorales@dcatech.com>
2020-12-30 10:27:16 -0500

    Clase 94: Caching

--- api/src/routes/idea.routes.js
+++ api/src/routes/idea.routes.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
+const { ParseIntMiddleware } = require('../middlewares');
 
 module.exports = function({IdeaController}){
   const router = Router();
 
-  router.get('', IdeaController.getAll);
+  router.get('', ParseIntMiddleware, IdeaController.getAll);
   router.post('', IdeaController.create);
   router.get('/:userId/all', IdeaController.getUserIdeas);
   router.get('/:ideaId', IdeaController.get);
