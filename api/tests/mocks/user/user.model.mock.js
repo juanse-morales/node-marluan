edbe7caeb902a770c494d0ba28752a5f687249b3
Juan Morales <jmorales@dcatech.com>
2021-01-04 11:51:29 -0500

    Clase 95: Implementando unit testing

---
+++ api/tests/mocks/user/user.model.mock.js
@@ -0,0 +1,22 @@
+module.exports = {
+  user: {
+    _id: "507f191e810c19729de860ea",
+    name: "Marluan",
+    username: "marluan03",
+    password: "mystrongPassword"
+  },
+  users:[
+    {
+      _id: "507f191e810c19729de860ea",
+      name: "Marluan",
+      username: "marluan03",
+      password: "mystrongPassword"
+    },
+    {
+      _id: "507f191e810c19729de860eb",
+      name: "Erick",
+      username: "erick_34",
+      password: "mystrongPassword"
+    }
+  ]
+};
